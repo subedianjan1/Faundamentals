71ab5744c21030237efa106597a41f1d5278eed2
Anj <subedianjan@yahoo.com.au>
2019-07-24 16:57:12 +1000

    Doc test basics and Moc tests comments

---
+++ Doc tests/DocTest.py
@@ -0,0 +1,15 @@
+"""
+'''All of the codes for Doc test inside doube inverted comma and everything looks like
+Commented. But this is how it works
+
+Use: python -m doctest -v DocTest.py
+
+'''
+>>> from Funcs import *
+
+>>> summ(2,6)
+8
+>>> summ( 'Hello', 'Life')
+'HelloLife'
+
+"""
---
+++ Doc tests/Funcs.py
@@ -0,0 +1,11 @@
+from unittest.mock import MagicMock
+import time
+
+def summ(a,b):
+    #print ( "Hello Mite")
+    time.sleep(1) #wait 1 Sec
+   # print ("Just waited 10 Secs")
+    return a + b
+
+
+
